80811525e229c1d45575fa8b473fbfee55c29fc1
ser0p <ser0p.nocomment@gmail.com>
2023-02-23 13:24:30 +0300

    Fix bugs

--- main.py
+++ main.py
@@ -1,58 +1,52 @@
-from fastapi import FastAPI, Request, Form, Depends
+# from schemas import MyForm
+from enum import Enum
 from typing import Optional
-from pydantic import BaseModel
+
+import uvicorn
+from fastapi import Depends, FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from schemas import MyForm
-from enum import Enum
-import uvicorn
-
+from pydantic import BaseModel
 from transformers import AutoConfig, pipeline
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates/")
 
-@app.get("/", response_class=HTMLResponse)
-def redirect_fastapi(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
 
-
-@app.get("/new_form", response_class=HTMLResponse)
-def post_form(request: Request):
-    result = "Type the begging of the joke"
+@app.get("/")
+def home(request: Request):
+    result = 0
     return templates.TemplateResponse("test_item.html", {"request": request, 'result': result})
 
 
-@app.post("/new_form", response_class=HTMLResponse)
-def post_form(request: Request, form_data : MyForm = Depends(MyForm.as_form)):
-    # print(form_data)
 
-    start_tokens = form_data.start_tokens
+@app.post("/", response_class=HTMLResponse)
+def post_form(request: Request, some_text : str = Form(...)):
+    print(some_text)
 
-    model_folder = 'small_gpt'
+    # model_folder = 'small_gpt'
 
-    generation_config = AutoConfig.from_pretrained(f'{model_folder}/config.json')
+    # generation_config = AutoConfig.from_pretrained(f'{model_folder}/config.json')
 
     shutnik = pipeline('text-generation', 
-                        model=model_folder,
+                        model="abletobetable/gpt-short-jokes",
 
                         num_beams=10,
                         early_stopping=True, 
                         no_repeat_ngram_size=2, 
                         num_return_sequences=3 ,   # num_means>=num_return_sequences
                         top_k=50, 
-                        top_p=0.90, 
+                        top_p=0.95, 
                         temperature=0.6,
                         # min_length=10,
                         max_length=20, #80
 
-                        tokenizer='sberbank-ai/rugpt3small_based_on_gpt2',
-                        config = generation_config
+                        tokenizer='sberbank-ai/rugpt3small_based_on_gpt2'
+                        # config = generation_config
                     )
     
-    result = shutnik(start_tokens)[0]['generated_text']
+    result : str = shutnik(some_text)[0]['generated_text']
 
     return templates.TemplateResponse("test_item.html", {"request": request, 'result': result})
